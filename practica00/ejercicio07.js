35d00cbec7c65b81a9f6390be17575aee8d5ea00
Jose Fuentes <jofuto29@gmail.com>
2021-09-17 03:25:13 +0200

    tipos estructurados terminado

--- practica00/ejercicio07.js
+++ practica00/ejercicio07.js
@@ -3,6 +3,8 @@
 //un objeto es un conjunto no ordenado de para clave/valor, el valor de
 //esas claves puede ser qualquier literal de tipo primitico, funcion u otro objeto:
 
+
+
 let person = {
     name: "Pitson",
     last_name: "hdman",
@@ -20,10 +22,64 @@ console.log(person)
 //tambien podemos crear objetos de manera dinamica, es decir podemos crear un objeto vacio e ir añadiendo propiedades:
 
 person.nuevaPropiedad ="hola soy nueva en este objeto"
+
 //otra manera de declarar una nueva propiedad, es con declaracacion array:
-person['propiedadNueva2']="soy nuevo ttoo"
+person['propiedadNueva2']="soy nuevo too"
+
 //de igual manera podemos modificar una propiedad ya existente
 person.name="piterson"
+
 //de igual manera que antes podemos modificar una propiedad con una notacion distinta:
 person['last_name']= "hardman"
+
 console.log(person)
+
+//mediante funciones por ejemplo podemos acceder a propiedades de los objetos, pasandole a la funcion dicho objeto como argumento
+
+function quienEsQuien(person){
+    console.log("nombre de la persona: " + person.name)
+}
+
+quienEsQuien(person)
+
+//pero que pasa cuando accedemos a un parametro que no esta definido:
+console.log("propiedad no definida: " + person.District) //undefined
+
+//EL OBJETO JSON
+//es un formato de texto utilizado para serializar ojetos cuando deben transmitirse por la red
+//cada identificador de propiedad esta encerrado entre comillas dobles
+//tenemos dos metodos:
+    // JSON.stringify(Objeto) --> convierte un objeto js en una cadena json
+    // JSON.parse(String)     --> convierte una cadena JSON en un objeto js
+
+console.log("vamos a convertir el objeto persona utilizado anteriormente en una cadena JSON")
+let objJson = JSON.stringify(person)
+console.log(objJson)
+
+console.log("revirtamos el cambio")
+let objJs = JSON.parse(objJson)
+console.log(objJs)
+
+//podemos recorrer mediante bucles un objeto, vemoas como hacerlo:
+//for(variable in objeto), otra manera de crear un bucle for
+//la variable adoptara el nombre de de la propiedad que esta recorriendo
+
+for(let i in person){
+    console.log("property " + i + ": " + person[i])
+}
+
+//como podemos ver podemos tratar a un objeto como si fuera un array
+//tambien podemos usar como un array un objeto dentro de un objeto
+function recorreObjetoInterno(objeto){
+    for(let i in objeto){
+        console.log("property adrees " + i + ": " + objeto[i])
+    }
+}
+console.log("------------------------------------------------")
+//mediante funciones:
+for(let i in person){
+    if(person[i] == person.adress){recorreObjetoInterno(person[i])}
+    else{
+        console.log("property " + i + ": " + person[i])
+    }
+}
