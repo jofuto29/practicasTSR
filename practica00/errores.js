fea36f164851fa9e162a269ed31e9c3f3da67b74
Jose Fuentes <tu@email>
2021-09-20 09:44:59 +0200

    parte 0 terminada

---
+++ practica00/errores.js
@@ -0,0 +1,22 @@
+//ERRORES SEMANTICOS
+
+function sum(A){
+    return A.reduce((x,y)=>x+y)
+}
+//la operacion reduce devuelve la suma de todos los elementos del array
+console.log(sum([2,3,5]))
+
+//los errores pueden vernir si por ejemplo no le pasamos un array al metodo sum
+//para evitar eso podemos hacer lo sigueinte.
+
+function sum2(A){
+    if(!(A instanceof Array)) 
+        throw "A must be Array"
+    else {return A.reduce((x,y)=>x+y)}
+}
+
+/*
+utilizaremos:
+    instanceof --> para objetos... array es un objeto
+    typeof --> para tipos primitivos
+*/
