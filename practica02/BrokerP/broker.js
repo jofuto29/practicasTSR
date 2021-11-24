9d7edde7b568e1636b93b6383a4e9e936448d108
JOSÉ FUENTES TORNERO <jofuetor@upv.edu.es>
2021-11-24 17:31:01 +0100

    nu se que pasa

--- practica02/BrokerP/broker.js
+++ practica02/BrokerP/broker.js
@@ -1,9 +1,16 @@
 const zmq = require('zeromq')
 let cli=[], req=[], workers=[];
-let stWorkers= {}
+
+let trabajadores= {}
 
 let sc = zmq.socket('router') // frontend -- cliente
 let sw = zmq.socket('router') // backend -- worker
+
+if (process.argv.length < 4 ) {
+    console.log("faltan parametros por la linea de ordenes");
+    process.exit(-1);
+}
+
 
 let portClient = process.argv[2];
 let portWorker = process.argv[3];
@@ -28,35 +35,44 @@ sc.on('message',(c,sep,m)=> { //recibimos mensaje clientem, parece que noo hace 
 
 
 sw.on('message',(w,sep,c,sep2,r)=> { //recibimos mensaje del worker
-    contador++;
-    //comprobbacion de que el worker no esta en el array
-    var incide = estWorker.indexOf(w);
-    if(indice !== 0){ console.log("se añade " + w); estWorker.push(w);}
-    else console.log(w + " ya este en el array");
-
-
-    if (c=='') {workers.push(w); return}
-    if (cli.length>0) { //si hay mas clienters se encia al trabajor a seguir haciendo peticiones
-        sw.send([w,'', cli.shift(),'',req.shift()])
-    } else {
-        workers.push(w)
+    if (c=='') //registramos trabajador
+    {
+        workers.push(w);
+        trabajadores[w] = 0;
+        exit;
     }
-    estWorker[w]++;
-    sc.send([c,'',r + contador])
+    else{
+        PeticionesAtendidas++;
+        if (cli.length>0) { //si hay mas clienters se encia al trabajor a seguir haciendo peticiones
+            sw.send([w,'', cli.shift(),'',req.shift()])
+            trabajadores[w]++;
+        } else {
+            workers.push(w)
+            trabajadores[w]++;
+        }
+
+        sc.send([c,'',r + contador])
+    }
 })
 
+
 setInterval(()=>{
-    console.log("peticiones atendidas: " + PeticionesAtendidas);
+    console.log("peticiones totales atendidas: " + PeticionesAtendidas);
     console.log("peticiones por cada worker : ")
-    for(let i in stWorkers){
-        console.log("worker: " + i + "peticiones atendidas " + stWorkers[i]);
+    console.log(trabajadores)
+    for(let i in trabajadores){
+        console.log("trabajador: " + i + " peticiones atendidas " + trabajadores[i]);
     }
 },5000)
+
+
 /**
  * metodo pop --Z elimina el último elemento de un arreglo mutando su estructura inicial y luego devuelve dicho elemento como respuesta.
  * metodo push --Z nuevos elementos que pasemos como parametros al final del arreglo
  * metodo shift --Z elmimina primer elemento
  * metodo unshift --Z añade elemento al principio aumentando longitud del array
+ * 
+ * split(0,2) --Z elimina desde el incide 0 hasta el indice 2
  */ 
 
 
--- practica02/BrokerP/worker.js
+++ practica02/BrokerP/worker.js
@@ -1,15 +1,23 @@
 const zmq = require('zeromq')
 let req = zmq.socket('req')
 
-let UrlBackend = process.argv[2]
-let nick = process.argv[3]
+
+if (process.argv.length < 4) {
+    console.log('no ay suficientes parametros en la linea de ordenes, porfabor vuelva a intentar')
+    process.exit(-1);
+}
+
+
+let Backend = process.argv[2]
+let id = process.argv[3]
 let resply = process.argv[4]
 
-req.identity= nick + process.pid
-req.connect(UrlBackend)
+req.identity= id;
+req.connect(Backend)
 
-req.on('message', (c,sep,msg)=> {
-    console.log("msj" + msg);
+req.on('message', function(c,sep,msg) { //worker recibe un mensaje
+    console.log("msj: " + msg);
     setTimeout(()=> { req.send([c,'', resply])}, 1000)
 })
-req.send(['','',''])
+
+req.send([' ',' ',' ',]);
